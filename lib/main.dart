0f62f3c4f48f77649a6feb39e43853dfc39d5f66
Arjit <arjitmishra42@gmail.com>
2022-05-19 20:00:10 +0530

    at line 184

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,14 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:geofence/map.dart';
-import 'package:geofence/map_gesture.dart';
+import 'dart:collection';
+import 'dart:io';
+import 'dart:math' as Math;
 import 'package:poly_geofence_service/poly_geofence_service.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter_platform_interface/src/types/location.dart'
+    as Latlong;
+import 'package:poly_geofence_service/models/lat_lng.dart' as latlong;
 
 void main() => runApp(const ExampleApp());
 
@@ -17,7 +22,23 @@ class ExampleApp extends StatefulWidget {
 }
 
 class _ExampleAppState extends State<ExampleApp> {
+  static final Completer<GoogleMapController> _controller = Completer();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: Latlong.LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  final Set<Polygon> _polygons = HashSet<Polygon>();
+  final Set<Polyline> _polyLines = HashSet<Polyline>();
+
+  bool _drawPolygonEnabled = false;
+  late final List<Latlong.LatLng> _userPolyLinesLatLngList = [];
+  bool _clearDrawing = false;
+  int? _lastXCoordinate, _lastYCoordinate;
+
   final _streamController = StreamController<PolyGeofence>();
+  static final List<latlong.LatLng> _geofence = [];
 
   // Create a [PolyGeofenceService] instance and set options.
   final _polyGeofenceService = PolyGeofenceService.instance.setup(
@@ -27,24 +48,6 @@ class _ExampleAppState extends State<ExampleApp> {
       statusChangeDelayMs: 10000,
       allowMockLocations: true,
       printDevLog: false);
-
-  // Create a [PolyGeofence] list.
-  final _polyGeofenceList = <PolyGeofence>[
-    PolyGeofence(
-      id: 'Park',
-      data: {
-        'address': 'Ranjhi',
-        'about':
-            'Mountain park known for its 129m-high observation tower, statues & stone monuments.',
-      },
-      polygon: <LatLng>[
-        const LatLng(21.218320, 79.874410),
-        const LatLng(23.808930, 69.446700),
-        const LatLng(23.173100, 80.033176),
-        const LatLng(23.213310, 80.057555),
-      ],
-    ),
-  ];
 
   // This function is to be called when the geofence status is changed.
   Future<void> _onPolyGeofenceStatusChanged(PolyGeofence polyGeofence,
@@ -118,9 +121,125 @@ class _ExampleAppState extends State<ExampleApp> {
             centerTitle: true,
           ),
           body: _buildContentView(),
+          floatingActionButton: FloatingActionButton(
+            onPressed: _toggleDrawing,
+            tooltip: 'Drawing',
+            child: Icon((_drawPolygonEnabled) ? Icons.cancel : Icons.edit),
+          ),
         ),
       ),
     );
+  }
+
+  _toggleDrawing() {
+    _clearPolygons();
+    setState(() => _drawPolygonEnabled = !_drawPolygonEnabled);
+  }
+
+  _onPanUpdate(DragUpdateDetails details) async {
+    // To start draw new polygon every time.
+    if (_clearDrawing) {
+      _clearDrawing = false;
+      _clearPolygons();
+    }
+
+    if (_drawPolygonEnabled) {
+      late double x, y;
+      if (Platform.isAndroid) {
+        // It times in 3 without any meaning,
+        // We think it's an issue with GoogleMaps package.
+        x = details.globalPosition.dx * 3;
+        y = details.globalPosition.dy * 3;
+      } else if (Platform.isIOS) {
+        x = details.globalPosition.dx;
+        y = details.globalPosition.dy;
+      }
+
+      // Round the x and y.
+      int xCoordinate = x.round();
+      int yCoordinate = y.round();
+
+      // Check if the distance between last point is not too far.
+      // to prevent two fingers drawing.
+      if (_lastXCoordinate != null && _lastYCoordinate != null) {
+        var distance = Math.sqrt(Math.pow(xCoordinate - _lastXCoordinate!, 2) +
+            Math.pow(yCoordinate - _lastYCoordinate!, 2));
+        // Check if the distance of point and point is large.
+        if (distance > 80.0) return;
+      }
+
+      // Cached the coordinate.
+      _lastXCoordinate = xCoordinate;
+      _lastYCoordinate = yCoordinate;
+
+      ScreenCoordinate screenCoordinate =
+          ScreenCoordinate(x: xCoordinate, y: yCoordinate);
+
+      final GoogleMapController controller = await _controller.future;
+      Latlong.LatLng latLoong = await controller.getLatLng(screenCoordinate);
+
+      try {
+        // Add new point to list.
+        _userPolyLinesLatLngList.add(latLoong);
+        _geofence.add(latlong.LatLng(x, y));
+
+        _polyLines.removeWhere(
+            (polyline) => polyline.polylineId.value == 'user_polyline');
+        _polyLines.add(
+          Polyline(
+            polylineId: const PolylineId('user_polyline'),
+            points: _userPolyLinesLatLngList,
+            width: 2,
+            color: Colors.blue,
+          ),
+        );
+
+        PolyGeofence(
+            id: 'Park',
+            data: {
+              'address': 'Ranjhi',
+              'about':
+                  'Mountain park known for its 129m-high observation tower, statues & stone monuments.',
+            },
+            polygon: _geofence);
+      } catch (e) {
+        print(" error painting $e");
+      }
+      setState(() {});
+    }
+  }
+
+  final _polyGeofenceList = <PolyGeofence>[];
+
+  _onPanEnd(DragEndDetails details) async {
+    // Reset last cached coordinate
+    _lastXCoordinate = 0;
+    _lastYCoordinate = 0;
+
+    if (_drawPolygonEnabled) {
+      _polygons
+          .removeWhere((polygon) => polygon.polygonId.value == 'user_polygon');
+      _polygons.add(
+        Polygon(
+          polygonId: const PolygonId('user_polygon'),
+          points: _userPolyLinesLatLngList,
+          strokeWidth: 2,
+          strokeColor: Colors.blue,
+          fillColor: Colors.blue.withOpacity(0.4),
+        ),
+      );
+      setState(() {
+        _clearDrawing = true;
+      });
+    }
+  }
+
+  _clearPolygons() {
+    setState(() {
+      _polyLines.clear();
+      _polygons.clear();
+      _userPolyLinesLatLngList.clear();
+    });
   }
 
   @override
@@ -136,17 +255,31 @@ class _ExampleAppState extends State<ExampleApp> {
         final updatedDateTime = DateTime.now();
         final content = snapshot.data?.toJson().toString() ?? '';
 
-        return ListView(
-          physics: const BouncingScrollPhysics(),
-          padding: const EdgeInsets.all(8.0),
-          children: [
-            Text('•\t\tPolyGeofence (updated: $updatedDateTime)'),
-            const SizedBox(height: 10.0),
-            Text(content),
-            const Align(
-                alignment: Alignment.bottomCenter,
-                child: SizedBox(height: 400, width: 400, child: GMap()))
-          ],
+        return SingleChildScrollView(
+          child: Column(children: [
+            ListView(
+              physics: const BouncingScrollPhysics(),
+              padding: const EdgeInsets.all(8.0),
+              children: [
+                Text('•\t\tPolyGeofence (updated: $updatedDateTime)'),
+                const SizedBox(height: 10.0),
+                Text(content),
+                GestureDetector(
+                  onPanUpdate: (_drawPolygonEnabled) ? _onPanUpdate : null,
+                  onPanEnd: (_drawPolygonEnabled) ? _onPanEnd : null,
+                  child: GoogleMap(
+                    mapType: MapType.normal,
+                    initialCameraPosition: _kGooglePlex,
+                    polygons: _polygons,
+                    polylines: _polyLines,
+                    onMapCreated: (GoogleMapController controller) {
+                      _controller.complete(controller);
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ]),
         );
       },
     );
